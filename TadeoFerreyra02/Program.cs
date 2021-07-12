94dcb640c59eb9cdf917c5724d40668e0d0bf417
Tadeoferreyra14 <tadeoferreyra14@gmail.com>
2021-07-12 20:35:23 -0300

    Agregar archivos de proyecto.

---
+++ TadeoFerreyra02/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace TadeoFerreyra02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int ladoA, ladoB, ladoC;
+            Console.WriteLine("Ingrese lado A");
+            ladoA = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese lado B");
+            ladoB = int.Parse (Console.ReadLine());
+            Console.WriteLine("Ingrese lado C");
+            ladoC = int.Parse(Console.ReadLine());
+
+            if (ladoA == ladoB & ladoB==ladoC)
+            {
+                Console.Write("Es un triangulo equilatero");
+            }
+            else if (ladoA != ladoB & ladoA != ladoC & ladoC != ladoB)
+            {
+                Console.Write("es un triangulo escaleno");
+            }
+            else if (ladoA == ladoC && ladoB!=ladoC &&ladoB!=ladoA || ladoA==ladoB && ladoC!=ladoA && ladoC!=ladoB || ladoC==ladoB && ladoA!=ladoC && ladoA != ladoB)
+            {
+                Console.Write("Es un triangulo isoseles ");
+            }
+            Console.ReadLine();
+            
+
+        }
+    }
+}
